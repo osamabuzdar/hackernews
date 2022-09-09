af2fd20c930cdbaf173a7e19e1b2c2747b9fc645
osama buzdar <engr.osamabuzdar@gmail.com>
2022-09-09 11:58:44 +0500

    Initial Commit

---
+++ hacker news/hacker_newsApp.swift
@@ -0,0 +1,20 @@
+//
+//  hacker_newsApp.swift
+//  hacker news
+//
+//  Created by Osama Buzdar on 09/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct hacker_newsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
